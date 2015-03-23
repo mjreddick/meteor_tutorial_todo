68f03e0a9ca0b8925059045723193dbf79ad9da6
Matthew Reddick <matthewjreddick@gmail.com>
2015-03-23 16:52:59 -0700

    add hide completed

--- simple-todos.js
+++ simple-todos.js
@@ -5,7 +5,21 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     tasks: function () {
-      return Tasks.find({}, {sort: {createdAt: -1}});
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      } else {
+        // Otherwise, return all of the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+    
+    incompleteCount: function () {
+      return Tasks.find({checked: {$ne: true}}).count();
     }
   });
 
@@ -24,7 +38,12 @@ if (Meteor.isClient) {
 
       // Prevent default form submit
       return false;
+    },
+
+    "change .hide-completed input": function (event) {
+      Session.set("hideCompleted", event.target.checked);
     }
+
   });
 
   Template.task.events({
